9d41c368ef12aee65a9ae11de46519bea975008c
zhangliustc <zlahu@foxmail.com>
2018-10-16 23:33:58 +0800

    initial commit

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php return array (
+  'debug' => true,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'flarum',
+    'username' => 'carman',
+    'password' => 'sh123451',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => 'carman',
+    'port' => '3306',
+    'strict' => false,
+  ),
+  'url' => 'http://139.198.18.211:8181',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
